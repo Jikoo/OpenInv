a0d16b9033993e710d8cee89903d9a432381f64d
XPYEX <50171612+0XPYEX0@users.noreply.github.com>
2023-06-17 13:24:25 +0800

    import  StandardCharsets
    import

--- plugin/src/main/java/com/lishid/openinv/util/lang/LanguageManager.java
+++ plugin/src/main/java/com/lishid/openinv/util/lang/LanguageManager.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
