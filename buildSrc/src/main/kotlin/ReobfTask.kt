cb137e0f1062c1786a5be299660c5e8a5baf2bae
Jikoo <jikoo.games@gmail.com>
2025-02-14 16:20:08 -0500

    Prefer JavaExecAction
    Safer(TM) than executing Java via ProcessBuilder

--- buildSrc/src/main/kotlin/ReobfTask.kt
+++ buildSrc/src/main/kotlin/ReobfTask.kt
@@ -4,11 +4,13 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.bundling.Jar
+import org.gradle.process.internal.ExecActionFactory
 import java.io.File
 import java.nio.file.Paths
-import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
-abstract class ReobfTask: Jar() {
+abstract class ReobfTask
+  @Inject constructor(private var execActionFactory: ExecActionFactory): Jar() {
 
   @get:Input
   open val spigotVersion: Property<String> = objectFactory.property(String::class.java)
@@ -41,7 +43,7 @@ abstract class ReobfTask: Jar() {
     val obfPath = inFile.resolveSibling(inFile.name.replace(".jar", "-${intermediaryClassifier.get()}.jar"))
 
     // https://www.spigotmc.org/threads/510208/#post-4184317
-    val specialSourceDir = specialSource.get().path
+    val specialSourceFile = specialSource.get()
     val repo = mavenLocal.get()
     val spigotDir = repo.resolve("org/spigotmc/spigot/$spigotVer/")
     val mappingDir = repo.resolve("org/spigotmc/minecraft-server/$spigotVer/")
@@ -49,27 +51,27 @@ abstract class ReobfTask: Jar() {
     // Remap original Mojang-mapped jar to obfuscated intermediary
     val mojangServer = spigotDir.resolve("spigot-$spigotVer-remapped-mojang.jar")
     val mojangMappings = mappingDir.resolve("minecraft-server-$spigotVer-maps-mojang.txt")
-    remapPartial(specialSourceDir, mojangServer, mojangMappings, inFile, obfPath, true)
+    remapPartial(specialSourceFile, mojangServer, mojangMappings, inFile, obfPath, true)
 
     // Remap obfuscated intermediary jar to Spigot and replace original
     val obfServer = spigotDir.resolve("spigot-$spigotVer-remapped-obf.jar")
     val spigotMappings = mappingDir.resolve("minecraft-server-$spigotVer-maps-spigot.csrg")
-    remapPartial(specialSourceDir, obfServer, spigotMappings, obfPath, archiveFile.get().asFile, false)
+    remapPartial(specialSourceFile, obfServer, spigotMappings, obfPath, archiveFile.get().asFile, false)
   }
 
-  private fun remapPartial(specialSource: String, serverJar: File, mapping: File, input: File, output: File, reverse: Boolean) {
-    // TODO replace this with a direct dependency later?
-    //   May not be a good idea without an isolated classloader.
-    //   Would allow us to get rid of specialSource property.
-    ProcessBuilder()
-      .command(
-        "java", "-cp", "$specialSource${File.pathSeparator}$serverJar",
-        "net.md_5.specialsource.SpecialSource", "--live",
-        "-i", input.path, "-o", output.path,
-        "-m", "$mapping",
-        if (reverse) "--reverse" else ""
-      )
-      .start().waitFor(60, TimeUnit.SECONDS)
+  private fun remapPartial(specialSourceFile: File, serverJar: File, mapping: File, input: File, output: File, reverse: Boolean) {
+    // May need a direct dependency on SpecialSource later to customize behavior.
+    val exec = execActionFactory.newJavaExecAction()
+    exec.classpath = project.files(specialSourceFile, serverJar)
+    exec.mainClass.value("net.md_5.specialsource.SpecialSource")
+    exec.args(
+      "--live",
+      "-i", input.path,
+      "-o", output.path,
+      "-m", "$mapping",
+      if (reverse) "--reverse" else ""
+    )
+    exec.execute().rethrowFailure()
   }
 
 }
