1a6f825d73f0f65113b6f2470bd3c399ada68a0b
Jikoo <jikoo.games@gmail.com>
2025-10-19 23:06:05 -0400

    Add nodes for behavior of accessing equal

--- plugin/src/main/java/com/lishid/openinv/OpenInv.java
+++ plugin/src/main/java/com/lishid/openinv/OpenInv.java
@@ -260,6 +260,17 @@ public class OpenInv extends FoliaWrappedJavaPlugin implements IOpenInv {
       return this.accessor.openInventory(player, inventory, viewOnly);
     }
 
+    AccessEqualMode accessMode;
+    if (Permissions.ACCESS_EQUAL_EDIT.hasPermission(player)) {
+      accessMode = AccessEqualMode.ALLOW;
+    } else if (Permissions.ACCESS_EQUAL_VIEW.hasPermission(player)) {
+      accessMode = AccessEqualMode.VIEW;
+    } else if (Permissions.ACCESS_EQUAL_DENY.hasPermission(player)) {
+      accessMode = AccessEqualMode.DENY;
+    } else {
+      accessMode = config.getAccessEqualMode();
+    }
+
     for (int level = 4; level > 0; --level) {
       String permission = "openinv.access.level." + level;
       // If the target doesn't have this access level...
@@ -273,12 +284,12 @@ public class OpenInv extends FoliaWrappedJavaPlugin implements IOpenInv {
       }
 
       // If the viewer doesn't have an equal access level or equal access is a denial, deny.
-      if (!player.hasPermission(permission) || config.getAccessEqualMode() == AccessEqualMode.DENY) {
+      if (!player.hasPermission(permission) || accessMode == AccessEqualMode.DENY) {
         return null;
       }
 
       // Since this is a tie, setting decides view state.
-      if (config.getAccessEqualMode() == AccessEqualMode.VIEW) {
+      if (accessMode == AccessEqualMode.VIEW) {
         viewOnly = true;
       }
       break;
