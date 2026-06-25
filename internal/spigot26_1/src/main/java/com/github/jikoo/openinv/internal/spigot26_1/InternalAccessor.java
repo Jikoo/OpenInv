4980fac83518551dddc3958a14f24d5ba742dab5
Jikoo <jikoo.games@gmail.com>
2026-06-25 14:34:02 -0400

    Fix placeholders on older Spigot
    helps to actually implement both parts of a change

--- internal/spigot26_1/src/main/java/com/github/jikoo/openinv/internal/spigot26_1/InternalAccessor.java
+++ internal/spigot26_1/src/main/java/com/github/jikoo/openinv/internal/spigot26_1/InternalAccessor.java
@@ -1,9 +1,12 @@
 package com.github.jikoo.openinv.internal.spigot26_1;
 
+import com.github.jikoo.openinv.internal.spigot26_1.container.slot.placeholder.PlaceholderLoader;
 import com.github.jikoo.openinv.internal.spigot26_1.player.PlayerManager;
 import com.lishid.openinv.util.lang.LanguageManager;
+import org.bukkit.configuration.ConfigurationSection;
 import org.jspecify.annotations.NullMarked;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @NullMarked
@@ -19,6 +22,17 @@ public class InternalAccessor extends com.github.jikoo.openinv.internal.spigot26
   @Override
   public PlayerManager getPlayerManager() {
     return manager;
+  }
+
+  @Override
+  public void reload(ConfigurationSection config) {
+    ConfigurationSection placeholders = config.getConfigurationSection("placeholders");
+    try {
+      // Reset placeholders to defaults and try to load configuration.
+      new PlaceholderLoader().load(placeholders);
+    } catch (Exception e) {
+      logger.log(Level.WARNING, "Caught exception loading placeholder overrides!", e);
+    }
   }
 
 }
